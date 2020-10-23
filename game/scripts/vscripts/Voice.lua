56b12d500651623b51caafe0d18bfeef4b167555
Gamma <37131904+itsnotdapro@users.noreply.github.com>
2020-10-23 20:51:19 +1300

    Create Voice.lua

---
+++ game/scripts/vscripts/Voice.lua
@@ -0,0 +1,7 @@
+--------------------------------------------------------------
+-- This script is used to handle all script audio playpack. --
+--------------------------------------------------------------
+
+function playSound(name)
+    EmitGloablSound(name)
+end
